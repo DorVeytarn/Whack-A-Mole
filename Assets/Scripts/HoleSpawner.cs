13246111785dbb06b93f2477ab4109273c286aa3
DorVeytarn <artemijkrasnodarov@gmail.com>
2020-08-28 00:37:27 +0300

    1. Добавил спавн дырок и возможность настраивать их количество

---
+++ Assets/Scripts/HoleSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleSpawner : MonoBehaviour
+{
+    [SerializeField] private LevelManager _levelManager;
+    [SerializeField] private GameObject _parentHoleColumn;
+    [SerializeField] private GameObject _rawTemplate;
+    [SerializeField] private GameObject _holeTemplate;
+
+    private int _rawAmount;
+    private int _currentHoleAmount;
+
+    private void Awake()
+    {
+        InitializeGrid();
+    }
+
+    private void InitializeGrid()
+    {
+        _rawAmount = _levelManager.HoleAmountInRaw.Length;
+
+        for (int i = 0; i < _rawAmount; i++)
+        {
+            var newRaw = Instantiate(_rawTemplate, _parentHoleColumn.transform);
+            _currentHoleAmount = _levelManager.HoleAmountInRaw[i];
+            for (int j = 0; j < _currentHoleAmount; j++)
+            {
+                var newHole = Instantiate(_holeTemplate, newRaw.transform);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private int[] _holeAmountInRaw;
+
+    public int[] HoleAmountInRaw => _holeAmountInRaw;
+}
